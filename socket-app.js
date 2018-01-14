a360c0eece4668ff6a91e0109ad3f4ca195237eb
unknown <569283998@qq.com>
2018-01-14 01:23:11 -0800

    merge get_info and find_room

--- socket-app.js
+++ socket-app.js
@@ -8,12 +8,12 @@ let socketApp = (IO,socket) =>{
   };
   //connect
   handlers.onConnect();
-  //find room
+  //find room and send rooms info
   socket.on('find_room',handlers.onFindRoom.bind(null,user,socket,IO));
   //send msg
   socket.on('send_msg',handlers.onSendMsg.bind(null,user,socket,IO));
   //get room info
-  socket.on('get_info',handlers.onGetRoomInfo.bind(null,user,socket,IO));
+  //socket.on('get_info',handlers.onGetRoomInfo.bind(null,user,socket,IO));
   //disconnect
   socket.on('disconnect',handlers.onDisconnect.bind(null,user,socket,IO));
 
